6d0aaf1b8c723b8d2141ba013b7f6e9e61360331
imonshah143 <167785526+imonshah143@users.noreply.github.com>
2024-05-03 12:06:27 +0600

    nodi.py

--- nodi.py
+++ nodi.py
@@ -521,7 +521,7 @@ if __name__=='__main__':
 	except:pass
 
 def Subscraption():
-	key1=open('/storage/emulated/0/android8.txt', 'r').read()
+	key1=open(/storage/emulated/0/android8.txt','r').read()
 	r1=requests.get("https://github.com/imonshah143/AXEL.txt/blob/main/AXEL.txt").text
 	if key1 in r1:
 		os.system('clear')
